64741d99cbb6cfdd3a85d2f70be87e1b65ed710f
jjeongdong <donghun3296@naver.com>
2024-02-21 02:57:34 +0900

    chore : 프로젝트 초기세팅

---
+++ src/main/java/com/example/securityoauth2jwt/SecurityOAuth2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityoauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOAuth2JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOAuth2JwtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/securityoauth2jwt/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.securityoauth2jwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/my")
+    public String myAPI() {
+
+        return "my route";
+    }
+}
