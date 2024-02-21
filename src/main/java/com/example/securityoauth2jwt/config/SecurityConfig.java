b13c9622f48ce8fabdb123440e4b29f1c49a4a10
jjeongdong <donghun3296@naver.com>
2024-02-21 13:29:09 +0900

    chore : JWT 설정 완료

--- src/main/java/com/example/securityoauth2jwt/config/SecurityConfig.java
+++ src/main/java/com/example/securityoauth2jwt/config/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.example.securityoauth2jwt.config;
 
+import com.example.securityoauth2jwt.jwt.JWTFilter;
+import com.example.securityoauth2jwt.jwt.JWTUtil;
+import com.example.securityoauth2jwt.oauth2.CustomSuccessHandler;
 import com.example.securityoauth2jwt.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +24,32 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         //csrf disable
         http
@@ -32,11 +63,16 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
+        //JWTFilter 추가
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
         //oauth2
         http
                 .oauth2Login((oauth2) -> oauth2
-                    .userInfoEndpoint((userInfoEndpointConfig) ->
-                            userInfoEndpointConfig.userService(customOAuth2UserService)));
+                        .userInfoEndpoint((userInfoEndpointConfig) ->
+                            userInfoEndpointConfig.userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler));
 
         //경로별 인가 작업
         http
--- src/main/java/com/example/securityoauth2jwt/dto/UserDTO.java
+++ src/main/java/com/example/securityoauth2jwt/dto/UserDTO.java
@@ -2,7 +2,6 @@ package com.example.securityoauth2jwt.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
 @Getter
 @Setter
